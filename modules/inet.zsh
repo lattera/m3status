09cf33cd21a717f23d6f2b866d9a6b0bf33631e8
Shawn Webb <shawn.webb@hardenedbsd.org>
2015-08-28 18:25:05 -0400

    Add multiple IPv4 address support. add IPv6 support.

--- modules/inet.zsh
+++ modules/inet.zsh
@@ -1,10 +1,27 @@
 function inet() {
 	local j=0
 	foreach iface in ${interfaces}; do
-		ip=$(ifconfig ${iface} inet | grep inet | awk '{print $2;}')
+		local ip=""
+		ifconfig ${iface} inet | grep inet | while read line; do
+			if [ ! -z ${ip} ]; then
+				ip="${ip} / "
+			fi
+
+			ip="$(echo ${line} | awk '{print $2;}')"
+		done
+
 		if [ -z "${ip}" ]; then
 			ip="NONE"
 		fi
+
+		ifconfig ${iface} inet6 | grep inet6 | while read line; do
+			local ip6=$(echo ${line} | awk '{print $2;}')
+			if [ ! -z "${ip6}" ]; then
+				if [ ! -z "${ip6%fe80*}" ]; then
+					ip="${ip} / ${ip6}"
+				fi
+			fi
+		done
 
 		if [ ${j} -gt 0 ]; then
 			echo ","
