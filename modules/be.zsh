dd990468fd52f234821be278dbdec0c9f2e13eea
Shawn Webb <shawn.webb@hardenedbsd.org>
2015-08-28 18:03:32 -0400

    Add ZFS boot environment module

---
+++ modules/be.zsh
@@ -0,0 +1,27 @@
+if [ -z "${activebe}" ]; then
+	# Ensure ${activebe} is set in global namespace
+	activebe=""
+fi
+
+function be() {
+	if [ -z ${activebe} ]; then
+		beadm list | while read line; do
+			active=$(echo ${line} | awk '{print $2;}' | grep N)
+			if [ ! -z "${active}" ]; then
+				activebe=$(echo ${line} | awk '{print $1;}')
+				break
+			fi
+		done
+	fi
+
+	if [ -z ${activebe} ]; then
+		return
+	fi
+
+	cat <<EOF
+		{
+			"name": "bootenv",
+			"full_text": "BE: ${activebe}"
+		}
+EOF
+}
