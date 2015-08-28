362bc37be3197afec37f76fa8fee43344694d759
Shawn Webb <shawn.webb@hardenedbsd.org>
2015-08-28 15:29:42 -0400

    Add power source info

--- modules/battery.zsh
+++ modules/battery.zsh
@@ -1,8 +1,21 @@
 function battery() {
+	local life=$(sysctl -n hw.acpi.battery.life)
+	local charging=$(sysctl -n hw.acpi.battery.state)
+
+	if [ ${charging} -eq 0 ]; then
+		charging="Power"
+	else
+		charging="Battery"
+	fi
+
 cat<<EOF
 {
 	"name": "battery",
-	"full_text": "Battery: $(sysctl -n hw.acpi.battery.life)%"
+	"full_text": "Battery: ${life}%"
+},
+{
+	"name": "charging",
+	"full_text": "Source: ${charging}"
 }
 EOF
 }
