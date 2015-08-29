bd15e6b26e0c79da1bc332ae9e6f9e540dd33bbb
Shawn Webb <shawn.webb@hardenedbsd.org>
2015-08-28 21:17:12 -0400

    Add bhyve module

---
+++ modules/bhyvevms.zsh
@@ -0,0 +1,29 @@
+function bhyvevms() {
+	local vms=""
+
+	foreach file in $(find /dev/vmm); do
+		file=${file/\/dev\/vmm/}
+		if [ -z "${file}" ]; then
+			continue
+		fi
+
+		file=${file/\//}
+
+		if [ ! -z ${vms} ]; then
+			vms="${vms} "
+		fi
+
+		vms="${vms}${file}"
+	done
+
+	if [ -z ${vms} ]; then
+		vms="No VMs running"
+	fi
+
+	cat<<EOF
+		{
+			"name": "Bhyve_VMs",
+			"full_text": "Running bhyve VMs: ${vms}"
+		}
+EOF
+}
