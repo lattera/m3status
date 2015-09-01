6b5eff396d1ddc029ce5f1521deb181244c2815e
Shawn Webb <shawn.webb@hardenedbsd.org>
2015-09-01 11:54:51 -0400

    Add Linux support

--- modules/loadavg.zsh
+++ modules/loadavg.zsh
@@ -2,10 +2,17 @@
 # License: 2-clause BSD
 
 function loadavg() {
-cat<<EOF
-{
-	"name": "loadavg",
-	"full_text": "Load: $(uptime | sed 's/.*load averages: //' | awk -F\, '{print $1 $2 $3}')"
-}
+    local avg=""
+    if [ $(uname) = "Linux" ]; then
+        avg=$(uptime | sed 's/.*load average: //' | awk -F\, '{print $1 $2 $3}')
+    else
+        avg=$(uptime | sed 's/.*load averages: //' | awk -F\, '{print $1 $2 $3}')
+    fi
+
+    cat<<EOF
+    {
+        "name": "loadavg",
+        "full_text": "Load: ${avg}"
+    }
 EOF
 }
